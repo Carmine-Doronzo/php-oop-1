29e478f2a1d6699c1138e026420b9176599f57f1
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-05-10 17:29:32 +0200

    Divide class in models

--- index.php
+++ index.php
@@ -1,41 +1,5 @@
 <?php
-require_once __DIR__.'/Models/Movie.php';
-require_once __DIR__.'/Models//Tvseries.php';
-$films = [];
-$series = [];
-class Production
-{
-
-    public $title;
-    public $lang;
-    public $vote;
-    //public $profits;
-
-    function __construct($_title, $_lang, $_vote,)
-    {
-        $this->title = $_title;
-        $this->lang = $_lang;
-        $this->vote = $_vote;
-        //$this->profits = $_profits;
-    }
-
-    // public function setProfits(Profits $_profits) 
-    // {
-    //     $this->profits = $_profits;
-    // }
-
-}
-
-$Star_Wars = new Movie('Star Wars', 'eng', 7,'1.000.000','90 min');
-$Pulp_Fiction = new Movie('Pulp Fiction', 'eng', 9,'10.000.000','104 min');
-$Inglorious_Bastards = new Movie('Bastardi senza gloria', 'eng', 10,'100.000.000','134 min');
-$Walking_dead = new TvSeries('The Walking Dead', 'eng', 10, 10);
-array_push($films, $Star_Wars, $Pulp_Fiction, $Inglorious_Bastards);
-array_push($series, $Walking_dead);
-
-//$films[]= $Star_Wars . $Pulp_Fiction . $Inglorious_Bastards; 
-
-//var_dump($films);
+require_once __DIR__.'/Models/Production.php';
 ?>
 
 <!DOCTYPE html>
